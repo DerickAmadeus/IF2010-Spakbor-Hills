729a48e93f36908d2385c499f47bc757828953ff
evanderruizhix <evanderruizhi@gmail.com>
2025-05-06 13:29:49 +0700

    First Push (msh banyak blm lengkap)

---
+++ Player.java
@@ -0,0 +1,116 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private String name;
+    private char gender;
+    private int energy;
+    private static int maxEnergy = 100;
+    private String farmName;
+    private /*NPC */ String partner = null;
+    private int gold;
+    // private Inventory inventory;
+    // private Point location;
+    private List<Recipe> ownedRecipe;
+
+    public Player(String name, char gender, String farmName) {
+        this.name = name;
+        this.gender = gender;
+        energy = maxEnergy;
+        this.farmName = farmName;
+        gold = 0;
+        // construct inventory
+        // construct point
+        ownedRecipe = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public /*NPC */ String getPartner() {
+        return partner;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    /*public Inventory getInventory() {
+        return inventory;
+    }*/
+
+    /*public Point getLocation() {
+        return location;
+    }*/
+
+    public List<Recipe> getRecipe() {
+        return ownedRecipe;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public void setEnergy(int energy) { // throws exception di kelas yang make
+        if (energy > maxEnergy) {
+            this.energy = maxEnergy;
+        } else if (energy < 0) { // lupa actual valuenya brp
+            this.energy = 0;
+        } else {
+            this.energy = energy;
+        }
+    }
+
+    public void setPartner(String partner) {
+        this.partner = partner;
+    } //NPC
+
+    public void setGold(int gold) { // throws exception di kelas yang make
+        this.gold = gold; // gold ga valid bakal dihandle di buying
+    }
+
+    /*public void setInventory() {
+        hdeh
+    }*/
+
+    /*public Point setLocation() {
+        ntr
+    }*/
+
+    public void setRecipe(String unlocked) {
+        for (int i = 0; i < ownedRecipe.size(); i++) {
+            if (ownedRecipe.get(i).equals(unlocked)) {
+                ownedRecipe.get(i).setUnlockInfo(true);
+            }
+        }
+    }
+
+    public void printInventory() {}
+
+    public void proposing() {}
+
+    public void marry() {}
+
+    public void visiting() {}
+
+    public void chatting() {}
+
+    public void gifting() {}
+
+    public void moving() {}
+
+    public void openInventory() {}
+}
---
+++ Recipe.java
@@ -0,0 +1,24 @@
+public class Recipe {
+    private String itemID;
+    private String name;
+    // private List<item> ingredients;
+    private boolean unlockInfo = false;
+
+    public Recipe(String itemID, String name) {
+        this.itemID = itemID;
+        this.name = name;
+        /*ingredients = new ArrayList<>()*/
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean getUnlockInfo() {
+        return unlockInfo;
+    }
+    
+    public void setUnlockInfo(boolean info) {
+        unlockInfo = info;
+    }
+}
