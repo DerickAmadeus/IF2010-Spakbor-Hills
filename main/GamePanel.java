b6a6a954823e0f4fd6d23d2e30305432ac3dbdcb
Derick Amadeus <164653687+DerickAmadeus@users.noreply.github.com>
2025-05-31 00:20:59 +0700

    Add : new map looks

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -191,10 +191,10 @@ public class GamePanel extends JPanel implements Runnable {
         // Dari Farm Map (ID 0) ke HouseMap (ID 3)
         // Area pemicu, Door
         //muncul di depan door rumah
-        transitions.add(new TransitionData(0, 5, 10, 1, 1, 3, 7, 12, false, tileSize));
+        transitions.add(new TransitionData(0, 23, 23, 1, 1, 3, 7, 12, false, tileSize));
         // No additional transitions needed here for background color change.
 
-        transitions.add(new TransitionData(3, 7, 13, 1, 1, 0, 5, 11, false, tileSize));
+        transitions.add(new TransitionData(3, 7, 13, 1, 1, 0, 23, 24, false, tileSize));
 
         //Farm Map ke NPC map and backwards
         transitions.add(new TransitionData(0, 30, 30, 1, 1, 4, 4, 5, false, tileSize));
